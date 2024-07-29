fd5b5769b222d6ebbd1b06b9435a9611a34871ba
Yusuf PALA <148575930+palayusuf@users.noreply.github.com>
2024-07-29 16:45:32 +0300

    regulation complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,19 +3,19 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         OddEven EvenControl = new OddEven();
-        int sayi = 10;
-        boolean ciftmi = EvenControl.OddOrEven(sayi);
+        int number = 10;
+        boolean ciftmi = EvenControl.OddOrEven(number);
         System.out.println(ciftmi);
 
         Calculator hm = new Calculator();
-        int sonuc = hm.division(25, 5);
-        System.out.println("Division: " + sonuc);
-        sonuc = hm.multiplication(25, 5);
-        System.out.println("Multiplication: " + sonuc);
-        sonuc = hm.addition(25, 5);
-        System.out.println("Addition: " + sonuc);
-        sonuc = hm.subtraction(25, 5);
-        System.out.println("Scd /path/to/your/project\nubtraction: " + sonuc);
+        int result = hm.division(25, 5);
+        System.out.println("Division: " + result);
+        result = hm.multiplication(25, 5);
+        System.out.println("Multiplication: " + result);
+        result = hm.addition(25, 5);
+        System.out.println("Addition: " + result);
+        result = hm.subtraction(25, 5);
+        System.out.println("Scd /path/to/your/project\nubtraction: " + result);
 
         stringexample str = new stringexample();
         String message = "Hi, today is a very nice day.";
