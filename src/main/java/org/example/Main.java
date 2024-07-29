6d560ec91d9ae364100128dcb2c01436147ab373
Yusuf PALA <148575930+palayusuf@users.noreply.github.com>
2024-07-29 17:41:30 +0300

    Regulation complate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,14 +8,14 @@ public class Main {
         System.out.println(ciftmi);
 
         Calculator hm = new Calculator();
-        int result = hm.division(25, 5);
+        int result = hm.divide(25, 5);
         System.out.println("Division: " + result);
-        result = hm.multiplication(25, 5);
+        result = hm.multiply(25, 5);
         System.out.println("Multiplication: " + result);
-        result = hm.addition(25, 5);
-        System.out.println("Addition: " + result);
-        result = hm.subtraction(25, 5);
-        System.out.println("Scd /path/to/your/project\nubtraction: " + result);
+        result = hm.add(25, 5);
+        System.out.println("Sum: " + result);
+        result = hm.subtract(25, 5);
+        System.out.println("Subtraction: " + result);
 
         stringexample str = new stringexample();
         String message = "Hi, today is a very nice day.";
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -5,31 +5,31 @@ public class CalculatorTest {
     @Test
     public void testAddition() {
         Calculator hm = new Calculator();
-        assertEquals(7, hm.addition(3, 4), "Toplama işlemi doğru değil");
-        assertEquals(-1, hm.addition(-3, 2), "Toplama işlemi doğru değil");
+        assertEquals(7, hm.add(3, 4), "Toplama işlemi doğru değil");
+        assertEquals(-1, hm.add(-3, 2), "Toplama işlemi doğru değil");
     }
 
     @Test
     public void testMultiplication() {
         Calculator hm = new Calculator();
-        assertEquals(12, hm.multiplication(3, 4), "Çarpma işlemi doğru değil");
-        assertEquals(-6, hm.multiplication(-3, 2), "Çarpma işlemi doğru değil");
+        assertEquals(12, hm.multiply(3, 4), "Çarpma işlemi doğru değil");
+        assertEquals(-6, hm.multiply(-3, 2), "Çarpma işlemi doğru değil");
     }
 
     @Test
     public void testDivision() {
         Calculator hm = new Calculator();
-        assertEquals(2, hm.division(10, 5), "Bölme işlemi doğru değil");
-        assertEquals(-3, hm.division(-9, 3), "Bölme işlemi doğru değil");
+        assertEquals(2, hm.divide(10, 5), "Bölme işlemi doğru değil");
+        assertEquals(-3, hm.divide(-9, 3), "Bölme işlemi doğru değil");
 
-        assertEquals(0, hm.division(10, 0), "Bölme sıfır ile bölünememeli");
+        assertEquals(0, hm.divide(10, 0), "Bölme sıfır ile bölünememeli");
     }
 
     @Test
     public void testSubtraction() {
         Calculator hm = new Calculator();
-        assertEquals(-1, hm.subtraction(3, 4), "Çıkarma işlemi doğru değil");
-        assertEquals(-5, hm.subtraction(-3, 2), "Çıkarma işlemi doğru değil");
+        assertEquals(-1, hm.subtract(3, 4), "Çıkarma işlemi doğru değil");
+        assertEquals(-5, hm.subtract(-3, 2), "Çıkarma işlemi doğru değil");
     }
 }
 
