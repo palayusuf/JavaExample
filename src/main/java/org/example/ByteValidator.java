db9f93be3b067c8865a43ceddc07dc67d5799fd5
Yusuf PALA <148575930+palayusuf@users.noreply.github.com>
2024-07-31 14:12:54 +0300

    Update ByteValidator.java

--- src/main/java/org/example/ByteValidator.java
+++ src/main/java/org/example/ByteValidator.java
@@ -2,15 +2,12 @@ package org.example;
 
 public class ByteValidator {
 
-    // Geçerli karakterleri saklamak için kullanılan özellik
     public String validCharacters;
 
-    // Constructor ile validCharacters özelliğini başlatıyoruz
     public ByteValidator(String validCharacters) {
         this.validCharacters = validCharacters;
     }
 
-    // Byte dizisini geçerli karakterlerle karşılaştıran örnek metot
     public boolean isValidByteArray(byte[] byteArray) {
         for (byte b : byteArray) {
             char c = (char) b;
