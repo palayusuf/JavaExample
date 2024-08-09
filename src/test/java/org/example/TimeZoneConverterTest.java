24f761bb0deae096e29c067a61e46f3c4d92599f
Yusuf PALA <148575930+palayusuf@users.noreply.github.com>
2024-08-09 11:03:53 +0300

    tests updated

--- src/test/java/org/example/TimeZoneConverterTest.java
+++ src/test/java/org/example/TimeZoneConverterTest.java
@@ -4,17 +4,16 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.time.ZoneOffset;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TimeZoneConverterTest {
 
     @Test
     public void testConvertInstantToNewYork() {
-        Instant fixedInstant = Instant.parse("2024-08-08T01:15:00Z");
+        Instant fixedInstant = Instant.parse("2024-08-08T05:15:00Z");
         ZonedDateTime zonedDateTime = fixedInstant.atZone(ZoneId.of("America/New_York"));
         LocalDateTime expected = LocalDateTime.of(2024, 8, 8, 1, 15);
         LocalDateTime actual = zonedDateTime.toLocalDateTime();
@@ -23,7 +22,7 @@ public class TimeZoneConverterTest {
 
     @Test
     public void testConvertInstantToLosAngeles() {
-        Instant fixedInstant = Instant.parse("2024-08-08T21:15:00Z");
+        Instant fixedInstant = Instant.parse("2024-08-08T04:15:00Z");
         ZonedDateTime zonedDateTime = fixedInstant.atZone(ZoneId.of("America/Los_Angeles"));
         LocalDateTime expected = LocalDateTime.of(2024, 8, 7, 21, 15);
         LocalDateTime actual = zonedDateTime.toLocalDateTime();
@@ -32,7 +31,7 @@ public class TimeZoneConverterTest {
 
     @Test
     public void testConvertInstantToBrussels() {
-        Instant fixedInstant = Instant.parse("2024-08-08T12:15:00Z");
+        Instant fixedInstant = Instant.parse("2024-08-08T10:15:00Z");
         ZonedDateTime zonedDateTime = fixedInstant.atZone(ZoneId.of("Europe/Brussels"));
         LocalDateTime expected = LocalDateTime.of(2024, 8, 8, 12, 15);
         LocalDateTime actual = zonedDateTime.toLocalDateTime();
@@ -41,7 +40,7 @@ public class TimeZoneConverterTest {
 
     @Test
     public void testConvertInstantToBangkok() {
-        Instant fixedInstant = Instant.parse("2024-08-08T20:15:00Z");
+        Instant fixedInstant = Instant.parse("2024-08-08T13:15:00Z");
         ZonedDateTime zonedDateTime = fixedInstant.atZone(ZoneId.of("Asia/Bangkok"));
         LocalDateTime expected = LocalDateTime.of(2024, 8, 8, 20, 15);
         LocalDateTime actual = zonedDateTime.toLocalDateTime();
